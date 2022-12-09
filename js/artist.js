c950e30ebe9adebc22fee18c4d572cb3c561cb65
Khahliso Motsoeneng <khamotso021@student.wethinkcode.co.za>
2022-12-09 18:38:11 +0200

    about-tabs

--- js/artist.js
+++ js/artist.js
@@ -1,3 +1,12 @@
-function myFunction() {
-    document.getElementById("demo").innerHTML = "Paragraph changed.";
-  }
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+function opentab(tabname){
+    for(tlink of tablinks){
+        tlink.classList.remove("active-link") //this removes the active link
+    }
+    for(tlink of tabcontents){
+        tlink.classList.remove("active-tab") //this removes the active tab
+    }
+    event.currentTarget.classList.add("active-link"); //addind pink line as you click tab
+    document.getElementById(tabname).classList.add("active-tab");
+}
